679b31cfe675f21bfb3d2fbea73102c48b3e37e1
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-08-02 16:46:04 +0200

    feat: version title

--- packages/starlight-changelogs/translations.ts
+++ packages/starlight-changelogs/translations.ts
@@ -2,9 +2,11 @@ export const Translations = {
   en: {
     'starlightChangelogs.pagination.next': 'Older versions',
     'starlightChangelogs.pagination.prev': 'Newer versions',
+    'starlightChangelogs.version.title': 'Version {{version}}',
   },
   fr: {
     'starlightChangelogs.pagination.next': 'Versions plus anciennes',
     'starlightChangelogs.pagination.prev': 'Versions plus récentes',
+    'starlightChangelogs.version.title': 'Version {{version}}',
   },
 }
