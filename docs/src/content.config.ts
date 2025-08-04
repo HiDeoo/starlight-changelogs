d812e4b6d1200a5e5d07eb5267f7ca9c2f073117
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-08-04 14:31:14 +0200

    feat: date ui

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -17,17 +17,17 @@ export const collections = {
           return title + ' processed'
         },
       },
-      // {
-      //   provider: 'github',
-      //   base: 'test2',
-      //   owner: 'hideoo',
-      //   repo: 'starlight-blog',
-      //   token: import.meta.env.GITHUB_API_TOKEN,
-      //   process: ({ title }) => {
-      //     // if (!title.endsWith('.0')) return
-      //     return title.replace(/^(?:starlight-blog@|v)/, '')
-      //   },
-      // },
+      {
+        provider: 'github',
+        base: 'test2',
+        owner: 'hideoo',
+        repo: 'starlight-blog',
+        token: import.meta.env.GITHUB_API_TOKEN,
+        process: ({ title }) => {
+          // if (!title.endsWith('.0')) return
+          return title.replace(/^(?:starlight-blog@|v)/, '')
+        },
+      },
     ]),
   }),
 }
