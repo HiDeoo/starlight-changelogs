586931e9ba5d725d1fa2b5dbdcdb68f7fac670d3
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-03-12 21:59:36 +0100

    chore: add todo issue link

--- packages/starlight-changelogs/libs/data.ts
+++ packages/starlight-changelogs/libs/data.ts
@@ -4,12 +4,14 @@ import { getCollection, type CollectionEntry, type ContentConfig } from 'astro:c
 export async function getChangelogs(filter?: (entry: ChangelogsCollectionEntry) => unknown) {
   // TODO: this casting is a workaround for Astro v6 regression where the collection entry type is not properly inferred
   // for custom loaders. Remove the casting once the issue is resolved in Astro.
+  // https://github.com/HiDeoo/astro-6-cc-loader-schema-types-repro
   const entries = (await getCollection('changelogs', filter)) as ChangelogsCollectionEntry[]
   return entries.toSorted((a, b) => a.data.index - b.data.index)
 }
 
 // TODO: this casting is a workaround for Astro v6 regression where the collection entry type is not properly inferred
 // for custom loaders. Replace it by `CollectionEntry<'changelogs'>` once the issue is resolved in Astro.
+// https://github.com/HiDeoo/astro-6-cc-loader-schema-types-repro
 export type ChangelogsCollectionEntry = Omit<CollectionEntry<'changelogs'>, 'data'> & {
   data: z.infer<
     Extract<Required<ContentConfig['collections']['changelogs']>, { type?: 'content_layer' }>['loader']['schema']
